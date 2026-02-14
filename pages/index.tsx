33531106bba97acf9ba0410369374c3415cd6842
JohnDelavey <105748571+JohnDelavey@users.noreply.github.com>
2026-02-14 01:12:53 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,9 @@
-import EditorComponent from '../components/Editor'
+import Editor from '../components/Editor';
 
 export default function Home() {
   return (
-    <main style={{padding:'2rem'}}>
-      <h1>✨ Lexora AI Writing Studio</h1>
-      <EditorComponent />
+    <main>
+      <Editor />
     </main>
-  )
+  );
 }
